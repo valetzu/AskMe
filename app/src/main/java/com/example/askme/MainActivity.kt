84ad4effa4ba5aa8c78d319fa80298d6e327db35
valetzu <valzerlazer@gmail.com>
2023-04-27 01:19:33 +0300

    Started implementing the start screen view. Added the logo image, created the buttons with corresponding icon images.

--- app/src/main/java/com/example/askme/MainActivity.kt
+++ app/src/main/java/com/example/askme/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.askme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonOpenExercise = findViewById<TextView>(R.id.btnOpenExercise)
+        val buttonSettings = findViewById<TextView>(R.id.btnSettings)
+
+        //TODO: improve the buttons on start view
+        // (connect each icon image and its button and adjust constraints)
+
+        buttonOpenExercise.setOnClickListener {
+            //TODO: open the first exercise view
+        }
+
+        buttonSettings.setOnClickListener{
+            //TODO: open the settings view
+        }
     }
 }
