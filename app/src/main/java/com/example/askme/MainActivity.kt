a487866d50f6ee2a931eeeb2369a97a3514c48be
valetzu <valzerlazer@gmail.com>
2023-05-05 23:24:21 +0300

    Adjusted and fixed both the Main Menu and Settings screen layout.

--- app/src/main/java/com/example/askme/MainActivity.kt
+++ app/src/main/java/com/example/askme/MainActivity.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import com.example.askme.exercise.ExerciseMain
+import com.example.askme.settings.SettingsScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +16,6 @@ class MainActivity : AppCompatActivity() {
         val buttonOpenExercise = findViewById<Button>(R.id.btnOpenExercise)
         val buttonSettings = findViewById<Button>(R.id.btnSettings)
         val buttonNewExercise = findViewById<Button>(R.id.btnNewExercise)
-
-        //TODO: improve the buttons on start view
-        // (connect each icon image and its button and adjust constraints)
 
         buttonOpenExercise.setOnClickListener {
             val intent = Intent(this, ExerciseMain::class.java)
