90ce7899be5df62dc5617de900ce304dd9682988
Artturi <heikkilaartturi@gmail.com>
2023-05-08 02:49:10 +0300

    Added toast messages for switches in settings

--- app/src/main/java/com/example/askme/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/askme/settings/SettingsScreen.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
 import com.example.askme.MainActivity
 import com.example.askme.R
 
@@ -15,6 +17,8 @@ class SettingsScreen : AppCompatActivity() {
         val buttonExit = findViewById<Button>(R.id.btnExit)
         val buttonInfo = findViewById<Button>(R.id.btnInfo)
         val buttonHelp = findViewById<Button>(R.id.btnHelp)
+        val darkModeSwitch = findViewById<Switch>(R.id.swEnableDarkMode)
+        val muteSwitch = findViewById<Switch>(R.id.swEnableSound)
 
         buttonExit.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -31,9 +35,16 @@ class SettingsScreen : AppCompatActivity() {
             startActivity(intent)
         }
 
+        darkModeSwitch?.setOnCheckedChangeListener({ _ , isChecked ->
+            val message = if (isChecked) "Dark mode:ON" else "Dark mode:OFF"
+            Toast.makeText(this@SettingsScreen, message,
+            Toast.LENGTH_SHORT).show()
+        })
 
-
-
-
+        muteSwitch?.setOnCheckedChangeListener({ _ , isChecked ->
+            val message = if (isChecked) "Muted" else "Unmuted"
+            Toast.makeText(this@SettingsScreen, message,
+                Toast.LENGTH_SHORT).show()
+        })
     }
 }
